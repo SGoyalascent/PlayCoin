a482496288d6990b52b8d1a70523357f51e07920
SGoyalascent <shubham@ascentspark.com>
2022-02-28 21:31:35 +0530

    Update

---
+++ Program/deploy.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include <time.h>
+#include <math.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+void main() {
+
+    char *file_names[25]={  "/opt/raida0/Data/",
+                            "/opt/raida1/Data/",
+                            "/opt/raida2/Data/",
+                            "/opt/raida3/Data/",
+                            "/opt/raida4/Data/",
+                            "/opt/raida5/Data/",
+                            "/opt/raida6/Data/",
+                            "/opt/raida7/Data/",
+                            "/opt/raida8/Data/",
+                            "/opt/raida9/Data/",
+                            "/opt/raida10/Data/",
+                            "/opt/raida11/Data/",
+                            "/opt/raida12/Data/",
+                            "/opt/raida13/Data/",
+                            "/opt/raida14/Data/",
+                            "/opt/raida15/Data/",
+                            "/opt/raida16/Data/",
+                            "/opt/raida17/Data/",
+                            "/opt/raida18/Data/",
+                            "/opt/raida19/Data/",
+                            "/opt/raida20/Data/",
+                            "/opt/raida21/Data/",
+                            "/opt/raida22/Data/",
+                            "/opt/raida23/Data/",
+                            "/opt/raida24/Data/"};
+
+  for(int i=0; i <25; i++) {
+
+    char cpdir_path[256];
+
+    strcpy(cpdir_path, "cp playcoin ");
+    strcat(cpdir_path,file_names[i]);
+    strcat(cpdir_path, "playcoin");
+    system(cpdir_path);
+  }
+
+}
