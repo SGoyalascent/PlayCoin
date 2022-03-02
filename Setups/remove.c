9c3557147ddbf868cdcda580b8d6e2ae13f53433
SGoyalascent <shubham@ascentspark.com>
2022-03-02 15:31:12 +0530

    Update

--- Setups/remove.c
+++ Setups/remove.c
@@ -12,38 +12,38 @@
 
 void main() {
 
-    char *file_names[25]={  "/opt/raida0/Data/playcoin",
-                            "/opt/raida1/Data/playcoin",
-                            "/opt/raida2/Data/playcoin",
-                            "/opt/raida3/Data/playcoin",
-                            "/opt/raida4/Data/playcoin",
-                            "/opt/raida5/Data/playcoin",
-                            "/opt/raida6/Data/playcoin",
-                            "/opt/raida7/Data/playcoin",
-                            "/opt/raida8/Data/playcoin",
-                            "/opt/raida9/Data/playcoin",
-                            "/opt/raida10/Data/playcoin",
-                            "/opt/raida11/Data/playcoin",
-                            "/opt/raida12/Data/playcoin",
-                            "/opt/raida13/Data/playcoin",
-                            "/opt/raida14/Data/playcoin",
-                            "/opt/raida15/Data/playcoin",
-                            "/opt/raida16/Data/playcoin",
-                            "/opt/raida17/Data/playcoin",
-                            "/opt/raida18/Data/playcoin",
-                            "/opt/raida19/Data/playcoin",
-                            "/opt/raida20/Data/playcoin",
-                            "/opt/raida21/Data/playcoin",
-                            "/opt/raida22/Data/playcoin",
-                            "/opt/raida23/Data/playcoin",
-                            "/opt/raida24/Data/playcoin"};
+    char *file_names[25]={  "/opt/raida0/playcoin",
+                            "/opt/raida1/playcoin",
+                            "/opt/raida2/playcoin",
+                            "/opt/raida3/playcoin",
+                            "/opt/raida4/playcoin",
+                            "/opt/raida5/playcoin",
+                            "/opt/raida6/playcoin",
+                            "/opt/raida7/playcoin",
+                            "/opt/raida8/playcoin",
+                            "/opt/raida9/playcoin",
+                            "/opt/raida10/playcoin",
+                            "/opt/raida11/playcoin",
+                            "/opt/raida12/playcoin",
+                            "/opt/raida13/playcoin",
+                            "/opt/raida14/playcoin",
+                            "/opt/raida15/playcoin",
+                            "/opt/raida16/playcoin",
+                            "/opt/raida17/playcoin",
+                            "/opt/raida18/playcoin",
+                            "/opt/raida19/playcoin",
+                            "/opt/raida20/playcoin",
+                            "/opt/raida21/playcoin",
+                            "/opt/raida22/playcoin",
+                            "/opt/raida23/playcoin",
+                            "/opt/raida24/playcoin"};
 
   for(int i=0; i <25; i++) {
     
     if (remove(file_names[i]) == 0) {
-        printf("The file is deleted successfully.");
+        printf("The file is deleted successfully\n.");
     } else {
-        printf("The file is not deleted.");
+        printf("The file is not deleted.\n");
     }
 
   }
